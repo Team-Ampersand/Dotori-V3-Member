c576734706296bf39547b6beadc6f995ead9e989
aesperer <piyrw9754@gmail.com>
2024-01-29 13:13:00 +0900

    create :: member entity, authority enum

---
+++ src/main/kotlin/ampersand/userservice/persistence/Authority.kt
@@ -0,0 +1,5 @@
+package ampersand.userservice.persistence
+
+enum class Authority {
+    ROLE_ADMIN, ROLE_MEMBER, ROLE_COUNCILLOR, ROLE_DEVELOPER
+}
---
+++ src/main/kotlin/ampersand/userservice/persistence/MemberEntity.kt
@@ -0,0 +1,40 @@
+package ampersand.userservice.persistence
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+import org.jetbrains.annotations.NotNull
+
+@Entity
+@Table(name = "member")
+class MemberEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    val id: Long,
+
+    @field:NotNull
+    val name: String,
+
+    @field:NotNull
+    val email: String,
+
+    @Column(columnDefinition = "TINYINT")
+    val grade: Int?,
+
+    @Column(name = "class_number", columnDefinition = "TINYINT")
+    val classNumber: Int?,
+
+    @Column(columnDefinition = "TINYINT")
+    val number: Int?,
+
+    @field:NotNull
+    @field:Enumerated(EnumType.STRING)
+    val authority: Authority
+)
