d65b9f43efd55e25eeea5ef678360457b727aded
aesperer <piyrw9754@gmail.com>
2024-03-26 08:54:46 +0900

    chore :: properties scan jwt, datasource 프로퍼티 등록

--- src/main/kotlin/ampersand/userservice/configuration/PropertiesScanConfig.kt
+++ src/main/kotlin/ampersand/userservice/configuration/PropertiesScanConfig.kt
@@ -1,8 +1,10 @@
 package ampersand.userservice.configuration
 
+import ampersand.userservice.infrastructure.datasource.DataSourceProperties
+import ampersand.userservice.infrastructure.security.jwt.JwtProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan(basePackageClasses = [DataSourceProperties::class, JwtProperties::class])
 class PropertiesScanConfig
