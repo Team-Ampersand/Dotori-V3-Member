4dd2953eed1b86f15216f9bd7e7d9592c43c2b92
aesperer <piyrw9754@gmail.com>
2024-04-11 11:03:24 +0900

    error response 변경

--- src/main/kotlin/ampersand/userservice/infrastructure/error/handler/ErrorResponse.kt
+++ src/main/kotlin/ampersand/userservice/infrastructure/error/handler/ErrorResponse.kt
@@ -1,10 +1,6 @@
 package ampersand.userservice.infrastructure.error.handler
 
 data class ErrorResponse(
-    val errorMessage: String
-) {
-    companion object {
-        fun of(cause: Throwable): ErrorResponse =
-            ErrorResponse(cause.message!!)
-    }
-}
+    val errorMessage: String,
+    val status: Int
+)
